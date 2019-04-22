5c3de5047ff25d4a531e798eb57aa0e8e2c0f9cd
JustD4nTe <Domikxf@gmail.com>
2019-04-22 17:03:05 +0200

    Refactore

--- Snake_SFML/main.cpp
+++ Snake_SFML/main.cpp
@@ -5,18 +5,24 @@
 
 #define SNAKE_SIZE 20
 
+
+// Window size
 #define RES_X 500
 #define RES_Y 500
+
+// Snake start posision
+#define SNAKE_X 260
+#define SNAKE_Y 260
 
 #define RED 20
 #define GREEN 10
 #define BLUE 20
 
-sf::Vector2f newFruit(sf::RenderWindow &win);
+sf::Vector2f GeneratePositionForNewFruit(const sf::RenderWindow &win);
 
-void Move(sf::RectangleShape* snake[], unsigned int length);
-void Draw(sf::RenderWindow &win, sf::RectangleShape* snake[], unsigned int length);
-bool Collision(sf::RectangleShape* snake[], unsigned int length);
+void Move(sf::RectangleShape* snake[], const unsigned length);
+void Draw(sf::RenderWindow &win, sf::RectangleShape* snake[], const unsigned length);
+bool Collision(sf::RectangleShape* snake[], const unsigned length);
 
 int main() {
 	sf::RenderWindow Window(sf::VideoMode(RES_X, RES_Y), "Snake by JustD4nTe");
@@ -24,19 +30,24 @@ int main() {
 	sf::RectangleShape Fruit(sf::Vector2f(SNAKE_SIZE, SNAKE_SIZE));
 	sf::Clock timer;
 
+	const uint32_t fruitColor = 0xFF6633;
+
 	unsigned int SnakeLength = 1;
 	unsigned int Direction = 1;
 
 	unsigned int Green = 60, GreenCounter = 1;
+	// When Green was changed more than 20 times
+	// its goes darker or lighter (oppositly compare with last changing)
 	bool isReverseGreen = false;
 
 	float SnakeNextMove = 100, SnakeMoveTimeCounter = 0, SnakeSpeed = 400;
 
+	// Create head of snake
 	SnakeElements[0] = new sf::RectangleShape(sf::Vector2f(SNAKE_SIZE, SNAKE_SIZE));
-	SnakeElements[0]->setPosition(260, 260);
+	SnakeElements[0]->setPosition(SNAKE_X, SNAKE_Y);
 
-	Fruit.setFillColor(sf::Color(0xFF, 0x66, 0x33));
-	Fruit.setPosition(newFruit(Window));
+	Fruit.setFillColor(sf::Color(fruitColor));
+	Fruit.setPosition(GeneratePositionForNewFruit(Window));
 
 	while (Window.isOpen()) {
 		if (Collision(&SnakeElements[0], SnakeLength))
@@ -74,7 +85,7 @@ int main() {
 
 
 		SnakeMoveTimeCounter += SnakeSpeed * timer.restart().asSeconds();
-
+		// Moving
 		if (SnakeMoveTimeCounter >= SnakeNextMove) {
 			if (SnakeLength > 1)
 				Move(SnakeElements, SnakeLength);
@@ -98,13 +109,13 @@ int main() {
 
 			SnakeMoveTimeCounter = 0;
 		}
-
+		// Eating a fruit
 		if (Fruit.getPosition() == SnakeElements[0]->getPosition()) {
-			Fruit.setPosition(newFruit(Window));
+			Fruit.setPosition(GeneratePositionForNewFruit(Window));
 
 			SnakeElements[SnakeLength] = new sf::RectangleShape(sf::Vector2f(SNAKE_SIZE, SNAKE_SIZE));
 			SnakeElements[SnakeLength]->setPosition(SnakeElements[SnakeLength - 1]->getPosition().x, SnakeElements[SnakeLength - 1]->getPosition().y);
-			//std::cout << "SnakeLength: " << SnakeLength << "\tGreen: " << Green << std::endl;
+			
 			if (GreenCounter == 20) {
 				isReverseGreen = (isReverseGreen ? false : true);
 				GreenCounter = 1;
@@ -140,6 +151,7 @@ int main() {
 	text.setString(strEndGame);
 	text.setCharacterSize(24); 
 	text.setFillColor(sf::Color::White);
+
 	while (WinEndGame.isOpen()) {
 		sf::Event event;
 		while (WinEndGame.pollEvent(event)) {
@@ -154,31 +166,31 @@ int main() {
 	return 0;
 }
 
-sf::Vector2f newFruit(sf::RenderWindow &win) {
+sf::Vector2f GeneratePositionForNewFruit(const sf::RenderWindow &win) {
 	srand(static_cast<unsigned int>(time(NULL)));
 
-	unsigned int x = (rand() % (win.getSize().x / 20) + 0) * 20;
-	unsigned int y = (rand() % (win.getSize().y / 20) + 0) * 20;
+	unsigned x = (rand() % (win.getSize().x / 20) + 0) * 20;
+	unsigned y = (rand() % (win.getSize().y / 20) + 0) * 20;
 
 	return sf::Vector2f(x, y);
 }
 
-void Move(sf::RectangleShape* snake[], unsigned int length) {
+void Move(sf::RectangleShape* snake[],const unsigned int length) {
 	for (unsigned int i = length - 1 ; i > 0; --i)
 		snake[i]->setPosition(snake[i - 1]->getPosition());
 }
 
-void Draw(sf::RenderWindow &win, sf::RectangleShape* snake[], unsigned int length) {
-	for (int i = 1; i <= length; ++i)
+void Draw(sf::RenderWindow &win, sf::RectangleShape* snake[], const unsigned int length) {
+	for (unsigned int i = 1; i <= length; ++i)
 		win.draw(*snake[length - i]);
 }
 
-bool Collision(sf::RectangleShape* snake[], unsigned int length) {
+bool Collision(sf::RectangleShape* snake[], const unsigned int length) {
 	sf::RectangleShape HeadOfSnake = *snake[0];
 	if (HeadOfSnake.getPosition().x >= RES_X || HeadOfSnake.getPosition().x < 0 || HeadOfSnake.getPosition().y >= RES_Y || HeadOfSnake.getPosition().y < 0)
 		return true;
 
-	for (int i = 2; i < length; i++) {
+	for (unsigned int i = 2; i < length; i++) {
 		if (HeadOfSnake.getPosition() == snake[i]->getPosition())
 			return true;
 	}
